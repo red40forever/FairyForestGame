cc40b71be93defc03479cc138f9b2c6620500d02
eeglaser <glaser.emma.e@gmail.com>
2024-10-04 21:45:13 -0500

    Add camera panning

--- camera_2d.gd
+++ camera_2d.gd
@@ -1,11 +1,29 @@
 extends Camera2D
 
+var speed
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	speed = 200
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var direction := Vector2.ZERO
+	
+	# WASD controls
+	if Input.is_action_pressed("map_up"):
+		direction.y -= 1
+	if Input.is_action_pressed("map_down"):
+		direction.y += 1
+	if Input.is_action_pressed("map_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("map_right"):
+		direction.x += 1
+	
+	# Normalize the direction vector to maintain consistent speed
+	if direction != Vector2.ZERO:
+		direction = direction.normalized()
+	
+	# Move the camera
+	position += direction * speed * delta
