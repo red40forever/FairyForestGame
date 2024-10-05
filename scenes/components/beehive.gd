d929dc925a505aaa3d8e36be7654ec719b1880ed
eeglaser <glaser.emma.e@gmail.com>
2024-10-05 16:57:49 -0500

    Add day change logic
    TODO create and attach signal for this logic

--- scenes/components/beehive.gd
+++ scenes/components/beehive.gd
@@ -3,11 +3,13 @@ extends Node2D
 @export_group("Constraints")
 @export var max_bees: int = 3
 @export var max_storage: int = 5
+@export var new_bee_cost: int = 2
 
 # Pollen and honey share the same resource cap in beehives
 var current_pollen: int = 0
 var current_honey: int = 0
-var current_bees: int = 0
+var current_bees_total: int = 0 # how many bees belong to this hive
+var current_bees_at_home: int = 0 # how many bees are at home, aka not assigned to a task
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -22,3 +24,11 @@ func _input(event: InputEvent) -> void:
 	pass
 
 # TODO signal receiver from day change when day change signal is implemented
+func on_day_change():
+	# New bees created if possible
+	if current_honey >= new_bee_cost:
+		var bees_sum: int = current_bees_total + floor(current_honey / new_bee_cost)
+	
+	# New honey created by consuming pollen if possible
+	current_honey += current_pollen
+	current_pollen = 0
