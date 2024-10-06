a1aa727d9644c899cab64189dfbb436eb4adc60e
Owen Z <16374972+red40forever@users.noreply.github.com>
2024-10-06 17:15:17 -0500

    Fix ResourceSlot
    Also, rename ResourceSlot to Slot.

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -3,10 +3,16 @@ extends Node2D
 @export var camera: PanCamera
 @export var resource: GridObjectAttributes
 
-var honey: ResourceSlot
+var inventory: Slot # Slot usage example
 
 
 func _ready():
-	honey = ResourceSlot.new(ResourceType.HONEY, 20) # For example
+	# Slot usage example:
+	var accepted_resources: Array[Slot.ResourceType] = [
+		Slot.ResourceType.HONEY,
+		Slot.ResourceType.POLLEN
+	]
+	inventory = Slot.new(accepted_resources, 20)
+	
 	# debug
 	GameManager.tilemap_manager.create_object_at_coords(resource, Vector2i(5,5))
--- scripts/Slot.gd
+++ scripts/Slot.gd
@@ -1,11 +1,11 @@
-class_name ResourceSlot
+class_name Slot
 
 enum ResourceType {
 	HONEY,
 	POLLEN
 }
 
-var accepted_types: PackedInt32Array
+var accepted_types: Array[ResourceType]
 var stored_resources: Dictionary = {}
 var total_resource_count: int = 0
 var max_count: int = -1
@@ -15,8 +15,8 @@ signal resource_removed(resource_type: ResourceType, count: int)
 signal resource_count_updated(resource_type: ResourceType, count: int)
 
 
-func _init(new_type: int, new_max_count: int, new_count: int = 0):
-	self.type = new_type
+func _init(new_accepted_types: Array[ResourceType], new_max_count: int = -1):
+	self.accepted_types = new_accepted_types
 	for type in accepted_types:
 		self.stored_resources[type] = 0
 	self.max_count = new_max_count
@@ -25,7 +25,9 @@ func _init(new_type: int, new_max_count: int, new_count: int = 0):
 func add_resource(resource_type: ResourceType, count: int):
 	assert(count > 0, "Count must be positive.")
 	assert(stored_resources.has(resource_type), "Slot cannot take this ResourceType.")
-	assert(total_resource_count + count <= max_count, "Count must not cause slot to exceed max count.")
+	
+	if max_count != -1:
+		assert(total_resource_count + count <= max_count, "Count must not cause slot to exceed max count.")
 	
 	stored_resources[resource_type] += count
 	total_resource_count += count
@@ -54,7 +56,8 @@ func set_resource_count(resource_type: ResourceType, count: int):
 	
 	var resource_count = stored_resources[resource_type]
 	var total_resource_count_after_set = total_resource_count - resource_count + count
-	assert(total_resource_count_after_set <= max_count, "Count must not cause slot to exceed max count.")
+	if max_count != -1:
+		assert(total_resource_count_after_set <= max_count, "Count must not cause slot to exceed max count.")
 	
 	stored_resources[resource_type] = count
 	total_resource_count = total_resource_count_after_set
