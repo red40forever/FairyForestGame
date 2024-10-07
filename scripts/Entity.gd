c2922dc638b29c111c579998dac25c4cafbbba98
eeglaser <glaser.emma.e@gmail.com>
2024-10-07 16:58:26 -0500

    Fix entity tween deceleration
    Entity no longer weirdly decelerates to a velocity asymptote near the end of its tween

--- scripts/Entity.gd
+++ scripts/Entity.gd
@@ -12,8 +12,9 @@ var idle: bool = true
 signal return_home(resources_to_deposit: int, entity_reference_to_free: Node)
 
 func _ready():
-	# TODO set home? or maybe the home tile itself sets this when it spawns the bee? idk
 	target = self.position
+	# TODO debug, remove later
+	set_new_target(Vector2i(5,5))
 
 func _process(delta: float) -> void:
 	if not idle:
@@ -23,23 +24,27 @@ func _process(delta: float) -> void:
 
 func _physics_process(delta: float) -> void:
 	if not idle:
-		var direction := target - self.position
-		direction = direction.normalized()
-		# TODO sprite faces direction of movement
-		var tween = get_tree().create_tween()
-		var tween_duration = 10 # TODO
-		tween.tween_property(self, "global_position", target, tween_duration)
+		pass
 
-# TODO how to receive a new target? signal? from where?
+# TODO how to receive a new target? signal? from where? from player input?
 func set_new_target(new_target: Vector2i):
 	idle = false
 	# convert tilemap coords into world coords
 	target = GameManager.tilemap_manager.ground_layer.map_to_local(new_target)
+	
+	# Create tween that moves sprite to target
+	var direction := target - self.position
+	direction = direction.normalized()
+	# TODO sprite faces direction of movement
+	var tween = get_tree().create_tween()
+	var tween_duration = 10 # TODO
+	tween.tween_property(self, "global_position", target, tween_duration)
+	# TODO call on_reached_target when tween ends
 
-func set_home(new_home: Node):
+func set_home(new_home: Node) -> void:
 	home = new_home
 
-func go_towards_home():
+func go_towards_home() -> void:
 	target = home.global_position
 
 # TODO determine what type of target we're at, do stuff accordingly
