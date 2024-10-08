7c9642e6dc992fb8d83580e3abea728da8e6c6fa
Owen Z <16374972+red40forever@users.noreply.github.com>
2024-10-08 18:54:37 -0500

    Rewrite SlotDisplay

--- scripts/Slot.gd
+++ scripts/Slot.gd
@@ -10,9 +10,7 @@ var stored_resources: Dictionary = {}
 var total_resource_count: int = 0
 var max_count: int = -1
 
-signal resource_added(resource_type: ResourceType, count: int)
-signal resource_removed(resource_type: ResourceType, count: int)
-signal resource_count_updated(resource_type: ResourceType, count: int)
+signal resource_count_updated(resource_type: ResourceType, old_count: int, new_count: int)
 
 
 func _init(p_accepted_types: Array[ResourceType], p_max_count: int = -1):
@@ -26,28 +24,18 @@ func add_resource(resource_type: ResourceType, count: int):
 	assert(count > 0, "Count must be positive.")
 	assert(stored_resources.has(resource_type), "Slot cannot take this ResourceType.")
 	
-	if max_count != -1:
-		assert(total_resource_count + count <= max_count, "Count must not cause slot to exceed max count.")
-	
-	stored_resources[resource_type] += count
-	total_resource_count += count
-	
-	resource_added.emit(resource_added, count)
-	resource_count_updated.emit(resource_added, count)
+	var new_count = stored_resources[resource_type] + count
+	set_resource_count(resource_type, new_count)
 
 
 func remove_resource(resource_type: ResourceType, count: int):
 	assert(count > 0, "Count must be positive.")
 	assert(stored_resources.has(resource_type), "Slot cannot take this ResourceType.")
 	
-	var resource_count = stored_resources[resource_type]
-	assert(resource_count - count >= 0, "Count must not cause slot count to be negative.")
+	var new_count = stored_resources[resource_type] - count
+	assert(new_count >= 0, "Count must not cause slot count to be negative.")
 	
-	stored_resources[resource_type] -= count
-	total_resource_count += count
-	
-	resource_removed.emit(resource_added, count)
-	resource_count_updated.emit(resource_added, count)
+	set_resource_count(resource_type, new_count)
 
 
 func set_resource_count(resource_type: ResourceType, count: int):
@@ -59,10 +47,11 @@ func set_resource_count(resource_type: ResourceType, count: int):
 	if max_count != -1:
 		assert(total_resource_count_after_set <= max_count, "Count must not cause slot to exceed max count.")
 	
+	var old_count = stored_resources[resource_type]
 	stored_resources[resource_type] = count
 	total_resource_count = total_resource_count_after_set
 	
-	resource_count_updated.emit()
+	resource_count_updated.emit(resource_type, old_count, count)
 
 
 func get_resource_count(resource_type: ResourceType):
--- scripts/SlotDisplay.gd
+++ scripts/SlotDisplay.gd
@@ -1,113 +1,119 @@
-extends Node2D
+extends Control
 
 var displayed_slot: Slot
 
+var horizontal_containers = { }
+
 var open: bool = false
 var animating: bool = false
+var original_position: Vector2
 
 signal animation_finished(open: bool)
 
 
 func _ready():
+	# Testing
 	var accepted_types: Array[Slot.ResourceType] = [
 		Slot.ResourceType.HONEY,
 		Slot.ResourceType.POLLEN
 	]
 	displayed_slot = Slot.new(accepted_types, 5)
-	displayed_slot.add_resource(Slot.ResourceType.HONEY, 2)
-	displayed_slot.add_resource(Slot.ResourceType.POLLEN, 3)
+	displayed_slot.add_resource(Slot.ResourceType.HONEY, 3)
+	displayed_slot.add_resource(Slot.ResourceType.POLLEN, 2)
 	
-	displayed_slot.resource_added.connect(_on_slot_resource_added)
-	displayed_slot.resource_removed.connect(_on_slot_resource_removed)
+	displayed_slot.resource_count_updated.connect(_on_slot_resource_count_updated)
+	
+	# For open/close animation
+	original_position = position
+	visible = false
+	
+	# Create a horizontal container for each resource
+	for resource: Slot.ResourceType in displayed_slot.stored_resources:
+		var horizontal_container = HBoxContainer.new()
+		horizontal_container.alignment = BoxContainer.ALIGNMENT_CENTER
+		%VBoxContainer.add_child(horizontal_container)
+		horizontal_containers[resource] = horizontal_container
+		
+		var resource_count = displayed_slot.stored_resources[resource]
+		for i in range(resource_count):
+			var resource_sprite = _create_resource_icon(resource)
+			horizontal_container.add_child(resource_sprite)
+
+
+func _process(_delta):
+	if Input.is_action_just_pressed("ui_up"):
+		set_open(true)
+	elif Input.is_action_just_pressed("ui_down"):
+		set_open(false)
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		displayed_slot.remove_resource(Slot.ResourceType.HONEY, 1)
+	if Input.is_action_just_pressed("ui_cancel"):
+		displayed_slot.add_resource(Slot.ResourceType.HONEY, 1)
+
+
+func set_open(new_open: bool):
+	if new_open && !open:
+		animate_display_open()
+	elif !new_open && open:
+		animate_display_close()
+	open = new_open
 
 
 func animate_display_open():
-	if open:
-		return
-	
-	animating = true
-	open = true
-	
-	var padding = 10
-	var stored_resource_count = displayed_slot.stored_resources.size()
-	var starting_position = Vector2(
-		0,
-		-round(stored_resource_count * padding),
-	)
-	var resource_sprites: Array[Sprite2D] = []
-	for resource: Slot.ResourceType in displayed_slot.stored_resources:
-		var resource_count = displayed_slot.stored_resources[resource]
-		starting_position.x = round(-((padding * resource_count) / 2) + padding * 0.5)
-		#starting_position.x = 0
-		for i in range(resource_count):
-			var resource_sprite = _create_resource_sprite(resource)
-			resource_sprite.scale = Vector2.ZERO
-			resource_sprite.position = starting_position
-			resource_sprites.append(resource_sprite)
-			
-			starting_position.x += padding
-		starting_position.y += padding
-	
-	for resource_sprite in resource_sprites:
-		var target_position = resource_sprite.position
-		resource_sprite.position = Vector2.ZERO
-		
-		var tween = get_tree().create_tween()
-		tween.set_ease(Tween.EASE_OUT)
-		tween.set_trans(Tween.TRANS_CUBIC)
-		tween.tween_property(resource_sprite, "position", target_position, 0.1)
-		tween.set_parallel()
-		tween.tween_property(resource_sprite, "scale", Vector2.ONE, 0.1)
-		
-		await get_tree().create_timer(0.05).timeout
-	
-	animating = false
-	animation_finished.emit(true)
+	position = original_position + Vector2(0, 4)
+	visible = true
+	var tween = get_tree().create_tween()
+	tween.set_ease(Tween.EASE_OUT)
+	tween.set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property(self, "position", original_position, 0.1)
+	tween.set_parallel()
+	tween.tween_property(self, "modulate", Color.WHITE, 0.1)
 
 
 func animate_display_close():
-	if !open:
-		return
+	var tween = get_tree().create_tween()
+	tween.set_ease(Tween.EASE_OUT)
+	tween.set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property(self, "position", position + Vector2(0, 4), 0.1)
+	tween.set_parallel()
+	tween.tween_property(self, "modulate", Color(Color.WHITE, 0), 0.1)
+	tween.set_parallel(false)
+	tween.tween_callback(func(): visible = false)
+
+
+func _add_resource_icons(resource: Slot.ResourceType, count: int):
+	var sprite = _create_resource_icon(resource)
+	var horizontal_container: HBoxContainer = horizontal_containers[resource]
+	for i in range(count):
+		horizontal_containers[resource].add_child(sprite)
 	
-	animating = true
-	open = false
+	horizontal_container.visible = true
+
+
+func _remove_resource_icons(resource: Slot.ResourceType, count: int):
+	var children_to_delete: Array[TextureRect] = []
+	var horizontal_container: HBoxContainer = horizontal_containers[resource]
+	for i in range(count):
+		var child_to_delete = horizontal_container.get_child(0)
+		child_to_delete.queue_free()
 	
-	print("closing children:")
-	for child in get_children():
-		print(child.name)
-	
-	for child: Node2D in get_children():
-		var tween = get_tree().create_tween()
-		tween.set_ease(Tween.EASE_OUT)
-		tween.set_trans(Tween.TRANS_CUBIC)
-		tween.tween_property(child, "position", Vector2.ZERO, 0.1)
-		tween.set_parallel()
-		tween.tween_property(child, "scale", Vector2.ZERO, 0.1)
-		tween.set_parallel(false)
-		tween.tween_callback(
-			func():
-				if is_instance_valid(child):
-					child.queue_free()
-		)
-		
-		#await get_tree().create_timer(0.05).timeout
-		
-	animating = false
-	animation_finished.emit(false)
+	if horizontal_container.get_children().size() - count == 0:
+		horizontal_container.visible = false
 
 
-func _on_slot_resource_added(resource: Slot.ResourceType, count: int):
-	pass
+func _on_slot_resource_count_updated(resource: Slot.ResourceType, old_count: int, new_count: int):
+	var count_change = new_count - old_count
+	if count_change > 0:
+		_add_resource_icons(resource, count_change)
+	else:
+		_remove_resource_icons(resource, abs(count_change))
 
 
-func _on_slot_resource_removed(resource: Slot.ResourceType, count: int):
-	pass
-
-
-func _create_resource_sprite(resource: Slot.ResourceType) -> Sprite2D:
-	var resource_sprite = Sprite2D.new()
+func _create_resource_icon(resource: Slot.ResourceType) -> TextureRect:
+	var resource_sprite = TextureRect.new()
 	var sprites = Resources.find("ui_resource_sprites")
-	add_child(resource_sprite)
 	resource_sprite.texture = sprites[resource]
 	resource_sprite.texture_filter = CanvasItem.TEXTURE_FILTER_NEAREST
+	
 	return resource_sprite
