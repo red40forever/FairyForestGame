92c961de9812cf9b1ea44c423cf5cc7621a0d33c
Aidan <expertsummer2002@gmail.com>
2024-10-09 18:16:44 -0500

    Add Beat Detection

--- scripts/audio_manager.gd
+++ scripts/audio_manager.gd
@@ -1,53 +1,58 @@
 extends Node
 
+@export var menuMusicEvent: EventAsset
 @export var overworldMusicEventA: EventAsset
 @export var overworldMusicEventB: EventAsset
 @export var DialogueMusicEventBee: EventAsset
 @export var DialogueMusicEventMole: EventAsset
 
-enum song {GAMEPLAY_A, GAMEPLAY_B, DIALOGUE_BEE, DIALOGUE_MOLE}
+enum song {MENU, GAMEPLAY_A, GAMEPLAY_B, DIALOGUE_BEE, DIALOGUE_MOLE}
 
 var musicInstance: EventInstance
 
-var teststate = 0
-var time = 0
+var beat_callable: Callable = Callable(self, "beat_callback")
+
+var beat_counter = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("audio 1")
-	play(song.GAMEPLAY_A)
-	teststate = 1
+	pass
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	time += delta
-	if(teststate == 1 and time > 3):
-		print("audio 2")
-		setParameter("volume", -1)
-		teststate = 2
-	if(teststate == 2	 and time > 6):
-		print("audio 3")
-		setParameter("volume", 1)
-		teststate = 3
-		
+	pass
 
 func play(songName: song):
 	if(musicInstance != null):
 		musicInstance.stop(FMODStudioModule.FMOD_STUDIO_STOP_ALLOWFADEOUT)
 		musicInstance.release()
-	
+
+	var shouldCallback = false
+
 	match songName:
 		song.GAMEPLAY_A:
 			musicInstance = FMODRuntime.create_instance(overworldMusicEventA)
+			shouldCallback = true
 		song.GAMEPLAY_B:
 			musicInstance = FMODRuntime.create_instance(overworldMusicEventB)
+			shouldCallback = true
 		song.DIALOGUE_BEE:
 			musicInstance = FMODRuntime.create_instance(DialogueMusicEventBee)
 		song.DIALOGUE_MOLE:
 			musicInstance = FMODRuntime.create_instance(DialogueMusicEventMole)
 
+	if shouldCallback:
+		beat_counter = 0
+		musicInstance.set_callback(beat_callable, FMODStudioModule.FMOD_STUDIO_EVENT_CALLBACK_TIMELINE_BEAT)
+
 	musicInstance.start()
-	
+
 func setParameter(parameterName: String, value: Variant):
 	FMODStudioModule.get_studio_system().set_parameter_by_name(parameterName, value, false)
-	
+
+func beat_callback(args):
+	if args.properties.beat:
+		beat_counter += 1
+		if(beat_counter == 5):
+			beat_counter = 1
+			print("bar!")
