3c608964221325a070268606a7e7b662f61b2391
eeglaser <glaser.emma.e@gmail.com>
2024-10-10 17:14:23 -0500

    Create tree and TreeManager

---
+++ scripts/TreeManager.gd
@@ -0,0 +1,25 @@
+class_name TreeManager
+extends Node2D
+
+@export var resource_requirements_by_stage: Array = [
+	{ "honey": 3, "pollen": 6, "mushrooms": 0 },
+	{ "honey": 3, "pollen": 12, "mushrooms": 8 }
+]
+@export var stage = 0 
+@export var ending_stage = 2 # when the stage becommes this, the player wins
+var slot_honey = Slot
+var slot_pollen = Slot
+var slot_mushrooms = Slot
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	create_new_slots()
+
+func create_new_slots():
+	slot_honey = Slot.new([Slot.ResourceType.HONEY], resource_requirements_by_stage[stage]["honey"])
+	slot_pollen = Slot.new([Slot.ResourceType.POLLEN], resource_requirements_by_stage[stage]["pollen"])
+	slot_honey = Slot.new([Slot.ResourceType.MUSHROOM], resource_requirements_by_stage[stage]["mushrooms"])
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
