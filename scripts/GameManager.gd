0b942dab2b5859a8a8725464ed67c925d32c68eb
Owen Z <16374972+red40forever@users.noreply.github.com>
2024-10-09 20:39:19 -0500

    Initial ItemPile implementation

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -1,5 +1,5 @@
 extends Node
 
-@onready var tilemap_manager = $"../MainScene/%Tilemap"
-@onready var player = $"../MainScene/%Player"
-@onready var day_manager = $"../MainScene/%DayCycleManager"
+@onready var tilemap_manager: TilemapManager = $"../MainScene/%Tilemap"
+@onready var player: Node2D = $"../MainScene/%Player"
+@onready var day_manager: DayCycleManager = $"../MainScene/%DayCycleManager"
