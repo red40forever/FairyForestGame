4c05d0c163f452ebd399fd708f6ca98cb3f649de
eeglaser <glaser.emma.e@gmail.com>
2024-10-10 16:36:14 -0500

    Define effortless tiles for Bee and Mole

--- scripts/Bee.gd
+++ scripts/Bee.gd
@@ -1,9 +1,4 @@
 class_name Bee
 extends Entity
 
-func try_interact_with_object(object: GridObject):
-	# TODO
-	if object is Beehive:
-		self.return_home.emit(self, slot)
-
 func get_class_name(): return "Bee"
--- scripts/Mushroom.gd
+++ scripts/Mushroom.gd
@@ -2,3 +2,7 @@ class_name Mushroom
 extends ResourceTile
 
 func get_class_name(): return "Mushroom"
+
+# TODO on day change
+func _try_upgrade_tier():
+	pass
