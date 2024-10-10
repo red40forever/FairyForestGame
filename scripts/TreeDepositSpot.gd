0adbcf8fc56e7ea0aa0a4e10d0d1856f994e286d
eeglaser <glaser.emma.e@gmail.com>
2024-10-10 17:43:21 -0500

    Define request_interaction logic in TreeDepositSpot

--- scripts/TreeDepositSpot.gd
+++ scripts/TreeDepositSpot.gd
@@ -1,12 +1,29 @@
 class_name TreeDepositSpot
 extends InteractableGridObject
 
+var slots
+var types = {"honey": Slot.ResourceType.HONEY, "pollen": Slot.ResourceType.POLLEN, 
+		"mushrooms": Slot.ResourceType.MUSHROOM}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var p = get_parent()
+	slots = {types["honey"]: p.slot_honey, "honey": p.slot_honey,
+	types["pollen"]: p.slot_pollen, "pollen": p.slot_pollen, 
+	types["mushrooms"]: p.slot_mushrooms, "mushrooms": p.slot_mushrooms}
 
 func get_class_name() -> String: return "TreeDepositSpot"
 
-func request_interaction(slot: Slot) -> bool:
-	return false # TODO
+func request_interaction(incoming_slot: Slot) -> bool:
+	for type in incoming_slot.accepted_types:
+		var resource_count = incoming_slot.get_resource_count(type)
+		if resource_count == 0:
+			continue
+		if types.has(type):
+			var slot = slots[type]
+			var overflow = slot.add_resource_overflow_safe(type, resource_count)
+			var exchange = slot.get_resource_count(type) - overflow
+			slot.remove_resource(type, exchange)
+			if exchange > 0:
+				return true
+	return false 
