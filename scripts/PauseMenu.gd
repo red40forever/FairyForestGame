9a3e714deb7393702c291141badee81f7c76a223
Owen Z <16374972+red40forever@users.noreply.github.com>
2024-10-09 23:37:12 -0500

    Pause menu pauses game

--- scripts/PauseMenu.gd
+++ scripts/PauseMenu.gd
@@ -1,3 +1,4 @@
+class_name PauseMenu
 extends Control
 
 
@@ -10,6 +11,9 @@ var state: State
 
 
 func _ready():
+	# Don't stop processing on pause
+	process_mode = Node.PROCESS_MODE_ALWAYS
+	
 	set_open(false)
 
 
@@ -17,8 +21,10 @@ func _process(_delta):
 	if Input.is_action_just_pressed("pause_game"):
 		if state == State.CLOSED:
 			set_state(State.PAUSED_MAIN)
+			GameManager.paused = true
 		else:
 			set_state(State.CLOSED)
+			GameManager.paused = false
 
 
 func set_state(new_state: State):
