42f81edbbfbee09974f83b53480db153046c4a1d
eeglaser <glaser.emma.e@gmail.com>
2024-10-10 19:27:00 -0500

    Mole digs new MoleHill on empty tiles

--- scripts/Mole.gd
+++ scripts/Mole.gd
@@ -1,6 +1,7 @@
 class_name Mole
 extends Entity
 
+@export var the_stupid_way: GridObjectAttributes
 
 func set_new_target(new_target: Vector2i):
 	super(new_target)
@@ -11,3 +12,8 @@ func _on_tween_finished():
 	main_sprite.play("idle")
 
 func get_class_name(): return "Mole"
+
+# Mole digs new MoleHill on empty tiles
+func interact_with_empty_tile():
+	var hill = GameManager.tilemap_manager.create_object_at_coords(the_stupid_way, grid_coordinates)
+	set_home(hill)
