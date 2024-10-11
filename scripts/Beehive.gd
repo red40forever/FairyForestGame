e417a46bec0764ac09916a0ee13d1ae7879589a5
eeglaser <glaser.emma.e@gmail.com>
2024-10-10 23:06:51 -0500

    Nodes with Slots now initialize their enums in ready

--- scripts/Beehive.gd
+++ scripts/Beehive.gd
@@ -2,10 +2,12 @@ class_name Beehive
 extends HomeTile
 
 func _ready():
+	entity_grid_object_attributes = Resources.find("objects")["bee"]
+	entity_attributes = Resources.find("entity_attributes")["bee"]
+	resource_type = Slot.ResourceType.POLLEN
+	product_type = Slot.ResourceType.HONEY
 	super()
 	if randf() > 0.5:
 		main_sprite.flip_h = true
-	entity_grid_object_attributes = Resources.find("objects")["bee"]
-	entity_attributes = Resources.find("entity_attributes")["bee"]
 
 func get_class_name(): return "Beehive"
--- scripts/MoleHill.gd
+++ scripts/MoleHill.gd
@@ -4,6 +4,8 @@ extends HomeTile
 func get_class_name(): return "MoleHill"
 
 func _ready() -> void:
-	super()
 	entity_grid_object_attributes = Resources.find("objects")["mole"]
 	entity_attributes = Resources.find("entity_attributes")["mole"]
+	resource_type = Slot.ResourceType.NULL
+	product_type = Slot.ResourceType.MUSHROOM
+	super()
