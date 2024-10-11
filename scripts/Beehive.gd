1c8ca704ec7f66c42bec779564c015b32aa58250
eeglaser <glaser.emma.e@gmail.com>
2024-10-10 22:16:11 -0500

    HomeTiles set respective resources at ready

--- scripts/Beehive.gd
+++ scripts/Beehive.gd
@@ -6,5 +6,6 @@ func _ready():
 	if randf() > 0.5:
 		main_sprite.flip_h = true
 	entity_grid_object_attributes = Resources.find("object")["bee"]
+	entity_grid_object_attributes = Resources.find("entity_attributes")["bee"]
 
 func get_class_name(): return "Beehive"
--- scripts/MoleHill.gd
+++ scripts/MoleHill.gd
@@ -2,3 +2,8 @@ class_name MoleHill
 extends HomeTile
 
 func get_class_name(): return "MoleHill"
+
+func _ready() -> void:
+	super()
+	entity_grid_object_attributes = Resources.find("object")["mole"]
+	entity_grid_object_attributes = Resources.find("entity_attributes")["mole"]
