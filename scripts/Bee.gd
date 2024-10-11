826eb75585af4fedef6bc1f3d6fa31d3e4267226
eeglaser <glaser.emma.e@gmail.com>
2024-10-10 19:24:03 -0500

    Bee creates ItemPile on empty tiles
    the stupid way

--- scripts/Bee.gd
+++ scripts/Bee.gd
@@ -1,8 +1,11 @@
 class_name Bee
 extends Entity
 
+@export var the_stupid_way: GridObjectAttributes
+
 func get_class_name(): return "Bee"
 
 # Bees create ItemPiles on empty tiles
 func interact_with_empty_tile():
-	pass
+	var pile = GameManager.tilemap_manager.create_object_at_coords(the_stupid_way, grid_coordinates)
+	pile.request_interaction(slot)
