44626236125562005a38a2d5633f7df148e4925d
Owen Z <16374972+red40forever@users.noreply.github.com>
2024-10-18 22:55:45 -0500

    Fix error when freeing Bee entity

--- scripts/BeeAnimation.gd
+++ scripts/BeeAnimation.gd
@@ -2,15 +2,21 @@ extends Sprite2D
 
 @export var bob_height: int = 4
 
+var tween: Tween
+
 
 func _ready():
-	var tween = get_tree().create_tween()
+	tween = get_tree().create_tween()
 	tween.set_trans(Tween.TRANS_SINE)
 	
 	var peak_pos = position - Vector2(0, bob_height)
 	tween.tween_method(set_position_rounded, position, peak_pos, 3.0)
 	tween.tween_method(set_position_rounded, peak_pos, position, 3.0)
 	tween.set_loops()
+
+
+func _exit_tree():
+	tween.kill()
 
 
 func set_position_rounded(pos: Vector2):
