e58226dafc6af2805994f34d0f4098adf9fd4557
Owen Z <16374972+red40forever@users.noreply.github.com>
2024-10-21 01:02:23 -0500

    Despawn ItemPile if interaction failed

--- scripts/Bee.gd
+++ scripts/Bee.gd
@@ -1,16 +1,20 @@
 class_name Bee
 extends Entity
 
-var the_stupid_way: GridObjectAttributes = Resources.find("objects")["item_pile"]
 
 func get_class_name(): return "Bee"
+
 
 func _ready() -> void:
 	carryable_resources = [Slot.ResourceType.HONEY, Slot.ResourceType.POLLEN, Slot.ResourceType.SPORE]
 	super()
 
+
 # Bees create ItemPiles on empty tiles
 func interact_with_empty_tile():
 	if not slot.is_empty():
-		var pile = GameManager.tilemap_manager.create_object_at_coords(the_stupid_way, grid_coordinates)
-		pile.request_interaction(slot)
+		var pile_resource = Resources.find("objects")["item_pile"]
+		var pile_object = GameManager.tilemap_manager.create_object_at_coords(pile_resource, grid_coordinates)
+		var result = pile_object.request_interaction(slot)
+		if !result:
+			pile_object.despawn()
